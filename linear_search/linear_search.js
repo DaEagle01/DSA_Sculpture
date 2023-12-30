6a70c14cd4f45debabd3a0a6fc3b1c384b48e464
Fahad Bin Karim <fbinkarim01@gmail.com>
2023-12-31 01:42:34 +0600

    implemented basic linear search

--- linear_search/linear_search.js
+++ linear_search/linear_search.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+const arrOfNumbers = [1, 21, 2, 13, 78, 56, 9, 45, 7833, 4, 89];
+const target = 56;
+const findIndex = (numbers, target) => {
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] === target) {
+            return `Target ${numbers[i]} found at index ${i}`;
+        };
+    };
+    return "Target not found!";
+};
+console.log(findIndex(arrOfNumbers, target));
