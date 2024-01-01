65fc2e55496fbab0983bf3c6d6c95510f3f64d31
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-01-01 22:46:26 +0600

    solved

--- leetcode/35_search_insert_position.js
+++ leetcode/35_search_insert_position.js
@@ -2,3 +2,33 @@
     35. Search Insert Position
     https://leetcode.com/problems/search-insert-position/description/
 */
+
+var searchInsert = function (numbers, target) {
+    let start = 0;
+    let end = numbers.length - 1;
+    let mid;
+
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        if (numbers[mid] === target) {
+            return mid
+        }
+        else if (numbers[mid] > target) {
+            end = mid - 1;
+        }
+        else if (numbers[mid] < target) {
+            start = mid + 1;
+        }
+    };
+
+    if (numbers[mid] < target) {
+        return mid + 1;
+    }
+    return mid;
+};
+
+// const nums = [1, 3, 5, 6];
+const nums = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+const target = 25;
+
+console.log(searchInsert(nums, target));
