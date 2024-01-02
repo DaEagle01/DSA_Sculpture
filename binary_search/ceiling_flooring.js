4e1e08722b3a7f948f3a33fbadcae03417183704
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-01-02 22:54:32 +0600

    bug fixed

--- binary_search/ceiling_flooring.js
+++ binary_search/ceiling_flooring.js
@@ -14,11 +14,11 @@ function getCeilingFlooring(arr, target) {
             end = mid - 1;
         }
     }
-    return { ceiling: mid, flooring: mid + 1 }
+    return { ceiling: arr[mid], flooring: arr[mid - 1] }
 }
 
 let arr = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 //        [ 0,  1,  2,  3,  4,  5,  6,  7,  8,   9];
-let target = 79;
-
+// Target must be between the smallest and the largest value of the array.
+let target = 55;
 console.log(getCeilingFlooring(arr, target));
