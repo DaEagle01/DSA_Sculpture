3a1a1f3385b97cf4a788c676d40b5703feefa1f8
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-01-05 20:22:06 +0600

    solved leetcode problem 540

---
+++ leetcode/540_single_non_duplicate_element.js
@@ -0,0 +1,33 @@
+/* 
+    540. Single Element in a Sorted Array
+    https://leetcode.com/problems/single-element-in-a-sorted-array/description/
+*/
+
+const singleNonDuplicate = (arr) => {
+    let start = 0;
+    let end = arr.length - 1;
+    let mid;
+    while (start <= end) {
+        mid = Math.floor((start + end) / 2);
+        const isEven = (mid + 1) % 2 === 0;
+        if (arr[mid] !== arr[mid + 1] && arr[mid] !== arr[mid - 1]) {
+            return arr[mid];
+        }
+        else if (arr[mid] !== arr[mid + 1] && isEven) {
+            start = mid + 1;
+        }
+        else if (arr[mid] === arr[mid + 1] && !isEven) {
+            start = mid + 1;
+        }
+        else if (arr[mid] !== arr[mid - 1] && isEven) {
+            end = mid - 1
+        }
+        else {
+            end = mid - 1
+        }
+    }
+    return arr[mid];
+};
+console.log(singleNonDuplicate([3, 3, 4, 4, 7, 7, 10, 11, 11]));
+// console.log(singleNonDuplicate([3, 3, 7, 7, 10, 11, 11]));
+// console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]));
