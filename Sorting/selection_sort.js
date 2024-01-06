b879a1ad573a4889cea4a2431b8521663b4cbc2f
Fahad Bin Karim <fbinkarim01@gmail.com>
2024-01-07 00:51:40 +0600

    reshaped folder structure, implemented sorting using bubble sort and selection sort

---
+++ Sorting/selection_sort.js
@@ -0,0 +1,15 @@
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let minIndex = i;
+        for (let j = i; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+    return arr;
+};
+console.log(selectionSort([13, 12, 36, 17, 3, 9, 5, 7, 6]));
